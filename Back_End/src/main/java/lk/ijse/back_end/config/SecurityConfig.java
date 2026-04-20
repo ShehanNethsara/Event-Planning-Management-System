ca53a418003228b8f4032e49f6697509c49114cd
Shehan Nethsara <Shehan.nethsara2003@gmail.com>
2026-04-20 14:21:07 +0530

    Update SecurityConfig with JwtFilter

--- Back_End/src/main/java/lk/ijse/back_end/config/SecurityConfig.java
+++ Back_End/src/main/java/lk/ijse/back_end/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.back_end.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,21 +8,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll() // Login/Register permit karanawa
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll() // Swagger allow
-                        .anyRequest().authenticated() // Anith okkoma protected
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class); // Filter eka add kala
 
         return http.build();
     }
