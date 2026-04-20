b32966bff0755d4c5dfbc8bc4679eec449587823
Shehan Nethsara <Shehan.nethsara2003@gmail.com>
2026-04-20 10:34:07 +0530

    Create UserRepository for database operations

--- Back_End/src/main/java/lk/ijse/back_end/repository/UserRepository.java
+++ Back_End/src/main/java/lk/ijse/back_end/repository/UserRepository.java
@@ -1,12 +1,6 @@
 package lk.ijse.back_end.repository;
 
-
-import lk.ijse.back_end.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
