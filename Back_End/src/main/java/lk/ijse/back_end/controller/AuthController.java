5c8d6da67530f36d918aa9499f919ddf423fdeed
Shehan Nethsara <Shehan.nethsara2003@gmail.com>
2026-04-20 21:59:08 +0530

    Implement auth

--- Back_End/src/main/java/lk/ijse/back_end/controller/AuthController.java
+++ Back_End/src/main/java/lk/ijse/back_end/controller/AuthController.java
@@ -10,17 +10,19 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@CrossOrigin
+@CrossOrigin // Frontend එකයි Backend එකයි අතර සම්බන්ධතාවයට මේක අනිවාර්යයි
 public class AuthController {
 
     @Autowired
     private AuthService authService;
 
+    // User කෙනෙක්ව register කරන Endpoint එක
     @PostMapping("/register")
     public ResponseEntity<UserDTO> register(@RequestBody UserDTO userDTO) {
-        return ResponseEntity.ok(authService.registerUser(userDTO));
+        return ResponseEntity.ok(authService.register(userDTO));
     }
 
+    // Login වෙලා JWT Token එක ලබාගන්නා Endpoint එක
     @PostMapping("/login")
     public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO loginRequest) {
         return ResponseEntity.ok(authService.login(loginRequest));
